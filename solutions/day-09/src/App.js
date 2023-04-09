4f6fdf8d711e79ae5188b5cf558f49d0d41d45b7
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-09 16:01:08 +0530

    intial commit

---
+++ solutions/day-09/src/App.js
@@ -0,0 +1,11 @@
+import ToggleState from "./ToggleState";
+function App() {
+  return (
+    <div>
+      <h1>This is State in React</h1>
+      <ToggleState />
+    </div>
+  );
+}
+
+export default App;
---
+++ solutions/day-09/src/ToggleState.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+class ToggleState extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Toggler Component</h2>
+      </div>
+    );
+  }
+}
+
+export default ToggleState;
